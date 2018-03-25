d371b196f5b40e0e3bcb420e8a15ee5d7cbb01f8
xpiqiu <1156559005@qq.com>
2018-03-25 21:43:57 +0800

    1

--- app/src/main/java/com/example/xpq/part1/MainActivity.java
+++ app/src/main/java/com/example/xpq/part1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.xpq.part1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import  java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String lan = Locale.getDefault().getLanguage();
+//获取区域
+        String country = Locale.getDefault().getCountry();
+//设置成简体中文的时候，getLanguage()返回的是zh,getCountry()返回的是cn.
     }
+
 }
